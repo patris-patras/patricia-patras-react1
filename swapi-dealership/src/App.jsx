72fcff787cba2378bed663103eb14191d2780c03
patris-patras <patras.patricia@gmail.com>
2021-10-14 22:11:21 +0300

    swapi dealerSHIP - part I - live

---
+++ swapi-dealership/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <header className="nav-bar-dark fixed-top bg-dark border-bottom border-warning">
+        <nav className="container d-flex justify-content-between"></nav>
+        <h1 className="display-6 text-warning">Swapi DealerSHIP</h1>
+
+        {/* Search */}
+      </header>
+
+      <main className="container mb-4">main content</main>
+
+      <footer className="container mb-4">footer</footer>
+    </>
+  );
+}
+
+export default App;
