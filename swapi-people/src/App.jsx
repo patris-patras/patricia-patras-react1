112a0c4939775d36d9ded5de1e5701817bd43350
patris-patras <patras.patricia@gmail.com>
2021-10-14 19:00:23 +0300

    api swapi-people

---
+++ swapi-people/src/App.jsx
@@ -0,0 +1,15 @@
+import { Component, Fragment } from 'react';
+
+const baseUrl = 'https://swapi.dev/api/people';
+
+class App extends Component {
+  state = {
+    people: [],
+  };
+
+  render() {
+    return <Fragment></Fragment>;
+  }
+}
+
+export default App;
