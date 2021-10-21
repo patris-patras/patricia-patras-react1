600dcac7a235faeeea1b4e02d6eccac88f1838bf
patris-patras <patras.patricia@gmail.com>
2021-10-21 22:19:06 +0300

    small update - swapi search.jsx

--- swapi-dealership/src/legacy/Search.jsx
+++ swapi-dealership/src/legacy/Search.jsx
@@ -1,8 +1,10 @@
 import { Component } from 'react';
+import { AppContext } from '../contexts/AppContext';
 
 const baseUrl = 'https://swapi.dev/api/vehicles';
 
 class Search extends Component {
+  static contextType = AppContext;
   state = {
     busy: false,
     searchTerm: '',
@@ -19,13 +21,21 @@ class Search extends Component {
       .then((response) => {
         return response.json();
       })
-      .then(({ results: films }) => {
+      .then(({ results }) => {
         this.setState({
           busy: false,
           searchTerm: '',
         });
 
-        this.props.onSearchResults(films);
+        this.context.dispatch({
+          type: 'setSearchResults',
+          payload: results,
+        });
+
+        this.context.dispatch({
+          type: 'setScreen',
+          payload: 'searchResults',
+        });
       });
   };
 
